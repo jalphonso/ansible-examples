f055c3853da636e16001c354287c82f88ea20541
Joe Alphonso <jalphonso@juniper.net>
2021-11-16 12:03:26 -0500

    added pyez-triage.sh

---
+++ pyez-triage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd pyez-triage
+docker run -it --rm \
+--add-host spine1:100.123.13.211 \
+--add-host spine2:100.123.13.212 \
+--add-host leaf1:100.123.13.213 \
+--add-host leaf2:100.123.13.214 \
+pyez-triage:1.0
